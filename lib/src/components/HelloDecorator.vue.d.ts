1793fa3f30532905bfdb7f4338cec128e2ca0aae
denggang <denggang@gamil.com>
2019-06-06 12:10:41 +0800

    setting .tsconfig > declaration:false

--- lib/src/components/HelloDecorator.vue.d.ts
+++
@@ -1,9 +0,0 @@
-import { Vue } from "vue-property-decorator";
-export default class HelloDecorator extends Vue {
-    name: string;
-    initialEnthusiasm: number;
-    enthusiasm: number;
-    increment(): void;
-    decrement(): void;
-    readonly exclamationMarks: string;
-}
